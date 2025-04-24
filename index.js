100402c9f0deef752a33732f5986e48d6c3399cb
Pedro Dantas <xpedroxdantasx@gmail.com>
2025-04-24 22:00:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Developed by .pedro_dantas."];
+const statusMessages = ["Developed by pedro.dantas."];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
